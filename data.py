791af2ae6c5b9938446ed25821322c9624107938
Pattussi <pattussi@hotmail.com>
2025-07-24 17:33:31 -0300

    third commit

--- data.py
+++ data.py
@@ -1,5 +1,5 @@
 # Arquivo com constantes
-URBAN_ROUTES_URL = ''
+URBAN_ROUTES_URL = 'https://cnt-7dd14164-3ea9-49f5-b30e-a2a54a7b8a40.containerhub.tripleten-services.com?lng=pt'
 ADDRESS_FROM = 'East 2nd Street, 601'
 ADDRESS_TO = '1300 1st St'
 PHONE_NUMBER = '+1 123 123 12 12'
